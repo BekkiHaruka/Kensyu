952850deeb4f6a7119af09d02f0a7abbff6c2860
BekkiHaruka <muses-c.8823@docomo.ne.jp>
2018-05-15 01:24:30 +0900

    select.sh完成

--- select.sh
+++ select.sh
@@ -3,8 +3,25 @@
 LIST=(A B C D E F G H I J)
 COUNTER=(0 0 0 0 0 0 0 0 0 0)
 
-function random(){
-  num=$(( $RANDOM % 10))
-  echo ${LIST[$num]}
-  count[$num]=$(( count[$num] + 1))
-}
+if [ $# -gt 1 ]; then
+  exit 1
+fi
+
+if [ $# -eq 0 ]; then
+	echo ${LIST[$(( $RANDOM % 10 ))]}
+	exit 0
+fi
+
+if [[ ! "$1" =~ ^[1-9][0-9]*$ ]]; then
+	exit 1
+fi
+
+for ((i=0; i < $1; i++)); do
+	RAN=$[$RANDOM%10]
+	echo ${LIST[$RAN]}
+	COUNTER[$RAN]=$[${COUNTER[$RAN]}+1]
+done
+
+for i in {0..9}; do
+	echo $i, ${COUNTER[$i]}
+done
